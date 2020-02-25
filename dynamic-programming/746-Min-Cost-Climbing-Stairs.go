32e1fab1b556dee3c75f5ac1cc87846dc5c6c244
qiqi <18611313085@163.com>
2020-02-25 18:49:07 +0800

    update

--- dynamic-programming/746-Min-Cost-Climbing-Stairs.go
+++ dynamic-programming/746-Min-Cost-Climbing-Stairs.go
@@ -1,5 +1,8 @@
 package dynamic_programming
 
+// you can either start from the step with index 0, or the step with index 1
+// ==> cost[0] = cost[0]
+// ==> cost[1] = cost[1]
 func minCostClimbingStairs(cost []int) int {
 	n := len(cost)
 	for i := 2; i < n; i++ {
@@ -9,6 +12,7 @@ func minCostClimbingStairs(cost []int) int {
 			cost[i] += cost[i-2]
 		}
 	}
+	// 走到n-1或n-2都可以直达top
 	if cost[n-1] < cost[n-2] {
 		return cost[n-1]
 	}
