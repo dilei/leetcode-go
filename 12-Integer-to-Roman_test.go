923681920aed10048822ed7ef9c88b46ff23bc6a
dilei <dilei@dangdang.com>
2020-03-16 19:24:54 +0800

    update

---
+++ 12-Integer-to-Roman_test.go
@@ -0,0 +1,15 @@
+package leetcode_go
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_intToRoman(t *testing.T) {
+	res := intToRoman(3)
+	res1 := intToRoman(4)
+	res2 := intToRoman(9)
+	res3 := intToRoman(58)
+	res4 := intToRoman(1994)
+	fmt.Println(res, res1, res2, res3, res4)
+}
