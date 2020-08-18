e76423816a84202e984c1637823423a20be92103
dilei <dilei@dangdang.com>
2020-08-18 16:40:59 +0800

    update

--- 109-sorted-to-bestBST.go
+++ 109-sorted-to-bestBST.go
@@ -1,22 +1,5 @@
 package leetcodego
 
-/*
- * Definition for singly-linked list.
- */
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-/*
- * Definition for a binary tree node.
- */
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func sortedListToBST(head *ListNode) *TreeNode {
 	if head == nil {
 		return nil
